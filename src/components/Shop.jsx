c2e561e2e39673f2b7c2636fd9dc9da66b23df2e
marufbroh <marufmd775@gmail.com>
2023-04-10 16:51:53 +0600

    handleAddToCart func added

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -5,10 +5,14 @@ import ProductCard from './Cards/ProductCard';
 const Shop = () => {
     const products = useLoaderData();
 
+    const handleAddToCart =(id) => {
+        console.log("🚀 ~ file: Shop.jsx:9 ~ handleAddToCart ~ id:", id)
+    }
+
     return (
         <div className='product-container'>
             {
-                products.map(product => <ProductCard key={product.id} product={product} />)
+                products.map(product => <ProductCard key={product.id} product={product} handleAddToCart={handleAddToCart} />)
             }
         </div>
     );
